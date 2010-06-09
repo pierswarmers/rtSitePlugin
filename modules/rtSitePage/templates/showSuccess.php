559bfdec27cff3ca64e30e12bbcb555720dd66b8
Piers Warmers <hello@pierswarmers.com>
2010-06-10 09:52:15 +1000

    Added frontend edit button system.

--- modules/rtSitePage/templates/showSuccess.php
+++ modules/rtSitePage/templates/showSuccess.php
@@ -1,5 +1,6 @@
 <?php use_helper('I18N', 'Date', 'rtText', 'rtForm', 'rtDate', 'rtSite') ?>
-<div class="rt-site-page-show">
+<div class="rt-site-page-show rt-admin-edit-tools-panel">
+  <?php echo link_to(__('Edit'), 'rtSitePageAdmin/edit?id='.$rt_site_page->getId(), array('class' => 'rt-admin-edit-tools-trigger')) ?>
   <?php include_partial('site_page', array('rt_site_page' => $rt_site_page, 'sf_cache_key' => $rt_site_page->getId())) ?>
   <dl class="rt-meta-data">
     <dt><?php echo __('Created') ?>:</dt>
