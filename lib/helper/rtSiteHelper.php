bcac8bd71d80247c5c3fb8e864a14db9784efc45
Torsten Hirsch <torsten@wranglers.com.au>
2010-06-17 15:54:54 +1000

    add limit_lower to  to adjust the level in the navigation

--- lib/helper/rtSiteHelper.php
+++ lib/helper/rtSiteHelper.php
@@ -72,6 +72,7 @@ function rt_site_page_map($rt_site_page = null, $options = array())
     $query = Doctrine::getTable('rtSitePage')->getQuery();
     $query->andWhere('page.display_in_menu = 1');
     $query->andWhere('page.level <= ?', $options['limit_upper']);
+    $query->andWhere('page.level >= ?', $options['limit_lower']);
     $tree = Doctrine::getTable('rtSitePage')->getDescendantsOfRoot($root_page, $query, true);;
   }
 
