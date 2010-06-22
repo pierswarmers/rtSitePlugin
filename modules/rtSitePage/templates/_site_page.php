e22f9e9a88b0f41ee320a49d3a4a57c2af5abee2
Torsten Hirsch <torsten@wranglers.com.au>
2010-06-22 14:58:18 +1000

    add naming convention to classes.

--- modules/rtSitePage/templates/_site_page.php
+++ modules/rtSitePage/templates/_site_page.php
@@ -1,5 +1,4 @@
 <h1><?php echo $rt_site_page->getTitle() ?></h1>
-
-<div class="rt-page-content clearfix">
-<?php echo markdown_to_html($rt_site_page->getContent(), $rt_site_page); ?>
+<div class="rt-container">
+  <?php echo markdown_to_html($rt_site_page->getContent(), $rt_site_page); ?>
 </div>
--- modules/rtSitePage/templates/showSuccess.php
+++ modules/rtSitePage/templates/showSuccess.php
@@ -1,13 +1,6 @@
 <?php use_helper('I18N', 'Date', 'rtText', 'rtForm', 'rtDate', 'rtSite') ?>
-<div class="rt-site-page-show rt-admin-edit-tools-panel">
+
+<div class="rt-site-page rt-show rt-primary-container rt-admin-edit-tools-panel">
   <?php echo link_to(__('Edit'), 'rtSitePageAdmin/edit?id='.$rt_site_page->getId(), array('class' => 'rt-admin-edit-tools-trigger')) ?>
   <?php include_partial('site_page', array('rt_site_page' => $rt_site_page, 'sf_cache_key' => $rt_site_page->getId())) ?>
-  <dl class="rt-meta-data">
-    <dt><?php echo __('Created') ?>:</dt>
-    <dd><?php echo time_ago_in_words_abbr($rt_site_page->getCreatedAt(), $sf_user->getCulture()) ?></dd>
-    <dt><?php echo __('Updated') ?>:</dt>
-    <dd><?php echo time_ago_in_words_abbr($rt_site_page->getUpdatedAt(), $sf_user->getCulture()) ?></dd>
-    <dt><?php echo __('Version') ?>:</dt>
-    <dd><?php echo $rt_site_page->version ?></dd>
-  </dl>
 </div>
