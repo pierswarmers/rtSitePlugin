3f9106aaca1ae8f629cc25c05eed8a3faa81e8fe
Piers Warmers <hello@pierswarmers.com>
2010-10-29 08:33:25 +1100

    Ugraded handling of headings and div structure.

--- modules/rtSitePage/templates/_site_page.php
+++ modules/rtSitePage/templates/_site_page.php
@@ -1,4 +1,6 @@
-<h1><?php echo $rt_site_page->getTitle() ?></h1>
-<div class="rt-container">
-  <?php echo markdown_to_html($rt_site_page->getContent(), $rt_site_page); ?>
-</div>
+<?php use_helper('rtText') ?>
+<?php slot('rt-title') ?>
+<?php echo $rt_site_page->getTitle() ?>
+<?php end_slot(); ?>
+
+<?php echo markdown_to_html($rt_site_page->getContent(), $rt_site_page); ?>
--- modules/rtSitePage/templates/showSuccess.php
+++ modules/rtSitePage/templates/showSuccess.php
@@ -1,6 +1,4 @@
 <?php use_helper('I18N', 'Date', 'rtText', 'rtForm', 'rtDate', 'rtSite') ?>
 
-<div class="rt-site-page rt-show rt-primary-container rt-admin-edit-tools-panel">
-  <?php echo link_to(__('Edit'), 'rtSitePageAdmin/edit?id='.$rt_site_page->getId(), array('class' => 'rt-admin-edit-tools-trigger')) ?>
-  <?php include_partial('site_page', array('rt_site_page' => $rt_site_page, 'sf_cache_key' => $rt_site_page->getId())) ?>
-</div>
+<?php echo link_to(__('Edit'), 'rtSitePageAdmin/edit?id='.$rt_site_page->getId(), array('class' => 'rt-admin-edit-tools-trigger')) ?>
+<?php include_partial('site_page', array('rt_site_page' => $rt_site_page, 'sf_cache_key' => $rt_site_page->getId())) ?>
