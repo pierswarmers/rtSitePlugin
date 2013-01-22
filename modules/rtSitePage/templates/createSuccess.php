ef33c2589ccd4fa6bbdd4234a0606a9bceb420ed
Piers Warmers <hello@pierswarmers.com>
2013-01-22 22:12:23 +1100

    Added better handling of root page.

--- modules/rtSitePage/templates/createSuccess.php
+++ modules/rtSitePage/templates/createSuccess.php
@@ -1,6 +1,9 @@
 <?php
 
-/** @var rtSitePage $rt_site_page */
+/**
+ * @var rtSitePage $rt_site_page
+ * @var sfWebRequest $sf_request
+ */
 
 use_helper('I18N','Date');
 
@@ -10,7 +13,7 @@ slot('rt-title', 'Page not found');
 
 <div class="rt-section-content">
     <p>The requested site page doesn't exist yet. Since you're an administrator of site pages - perhaps you meant for it to be here. Should we go ahead and create it now?</p>
-    <form action="<?php echo url_for($sf_request->getUri()) ?>" method="post">
+    <form action="<?php echo $sf_request->getPathInfo() ?>" method="post">
         <input type="hidden" name="slug" value="<?php echo $sf_request->getParameter('slug') ?>" />
         <p>
             <button type="submit"><?php echo __('Create page', null, 'sf_guard') ?></button>
